2bf346c5024269a6c31883a314e107c92b145b4e
Ranjit singh <jiradhey402@gmail.com>
2023-01-13 15:26:29 +0530

    problem solved

--- app.py
+++ app.py
@@ -4,21 +4,31 @@ import bs4
 import re
 
 app = Flask(__name__)
-url = "https://en.wikipedia.org/wiki/global warming"
 
-response = requests.get(url)
-soup = bs4.BeautifulSoup(response.text, "html.parser")
+@app.route('/')
+def Home():
+    return render_template('home.html')
 
-# Scraping the page title
-page_title = soup.find("h1", class_="firstHeading").text
+@app.route('/result',methods=['GET','POST'])
+def result():
+    if request.method == 'POST':
+        page_url = str(request.form['page_url'])
+        response = requests.get(page_url)
+        soup = bs4.BeautifulSoup(response.text, "html.parser")
 
-# Scraping all the paragraphs
-text = " "
-for p in soup.find_all("p"):
-    text += p.text
-#cleaned the text
-cleaned_text = re.sub("[^a-zA-Z-0-9]"," ",text)
+        # Scraping the page title
+        page_title = soup.find("h1", class_="firstHeading").text
 
+        # Scraping all the paragraphs
+        text = " "
+        for p in soup.find_all("p"):
+            text += p.text
+        # fetching starting character
+        # text = text[0:2500]
+
+        #cleaned the text
+        cleaned_text = re.sub("[^a-zA-Z-0-9]"," ",text)
+        return render_template('result.html',page_title=page_title,cleaned_text = cleaned_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
